b71f21e7da3bb1fab6b5286fd41b979ae7f28c24
Vinicius Ferreira <vini.g.fer@gmail.com>
2016-06-05 15:31:24 -0300

    - Removidos metodos nao utilizados - Ajustada identacao

--- src/source/BinaryTreeOfString.java
+++ src/source/BinaryTreeOfString.java
@@ -41,37 +41,6 @@ public class BinaryTreeOfString {
         return count;
     }
 
-    public String getRoot() throws Exception {
-        if (isEmpty()) {
-            throw new Exception("Árvore está vazia");
-        }
-        return root.element;
-    }
-
-    public void setRoot(String element) throws Exception {
-        if (isEmpty()) {
-            throw new Exception("Árvore está vazia");
-        }
-        root.element = element;
-    }
-    
-    public Integer getParent(Integer element) {
-        // Implementar
-        return null;
-    }
-
-    public int count() {
-        return count(root);
-    }
-    
-    private int count(Node n) {
-        if (n == null) {
-            return 0;
-        } else {
-            return 1 + count(n.left) + count(n.right);
-        }
-    }
-
     public int height() throws Exception {
         Queue<Node> fila = new Queue<>();
         Node aux = null;
@@ -104,58 +73,6 @@ public class BinaryTreeOfString {
         root = node;
         cursor = root;
         count++;
-        return true;
-    }
-
-    public boolean addLeft(String element, String father) {
-        Node n = searchNodeRef(father, root);
-        if (n == null)
-            return false;
-        if (n.left != null) 
-            return false;
-        Node left = new Node(element);
-        n.left = left;
-        left.father = n;
-        count++;
-        return true;
-        
-    }
-    
-    public boolean addRight(String element, String father) {
-        Node n = searchNodeRef(father, root);
-        if (n == null)
-            return false;
-        if (n.right != null) 
-            return false;
-        Node right = new Node(element);
-        n.right = right;
-        right.father = n;
-        count++;
-        return true;
-    }    
-
-    public boolean removeBranch(String element) {
-        Node n = this.searchNodeRef(element, root);
-        
-        if (n == null) {
-            return false;
-        }
-        
-        if (n == root) {
-            root = null;
-            count = 0;
-            return true;
-        }
-        
-        Node father = n.father;
-    
-        if (father.left == n) {
-            father.left = null;
-        } else {
-            father.right = null;
-        }
-        n.father = null;
-        count = count - this.count(n);
         return true;
     }
 
@@ -202,20 +119,6 @@ public class BinaryTreeOfString {
     
     public boolean verificaSeEstaNaRaiz() {
         return cursor == root;
-    }
-    
-    public Integer getLeft(Integer element) {
-        // Implementar
-        return null;
-    }
-
-    public String getRight(String element) {
-        // Implementar
-        Node n = this.searchNodeRef(element, root);
-        if (n != null)
-            if ( n.right != null)
-                return n.right.element;
-        return null;
     }
     
     public Double calcular() throws Exception  {
@@ -338,52 +241,5 @@ public class BinaryTreeOfString {
             }
         }
         return li;
-    }
-
-    public String strPositionsPre() {
-        return strPositionsPre(root);
-    }
-    private String strPositionsPre(Node n) {
-        String res = "";
-        // Implementar
-        return res;
-    }
-
-    public String strPositionsPos() {
-        return strPositionsPos(root);
-    }
-    private String strPositionsPos(Node n) {
-        String res = "";
-        // Implementar
-        return res;
-    }
-
-    public String strPositionsCentral() {
-        return strPositionsCentral(root);
-    }
-    private String strPositionsCentral(Node n) {
-        String res = "";
-        // Implementar
-        return res;
-    }
-
-    public boolean contains(String element) {
-        Node nAux = searchNodeRef(element, root);
-        return (nAux != null);
-    }
-
-    private Node searchNodeRef(String element, Node target) {
-        Node res = null;
-        if (target != null) {
-            if (target.element.equals(element)) {
-                res = target;
-            } else {
-                res = searchNodeRef(element, target.left);
-                if (res == null) {
-                    res = searchNodeRef(element, target.right);
-                }
-            }
-        }
-        return res;
     }
 }
--- src/source/Leitor.java
+++ src/source/Leitor.java
@@ -7,22 +7,22 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Leitor {
-	private File arquivo;
-	private BufferedReader reader;
+    private File arquivo;
+    private BufferedReader reader;
 	
-	public Leitor(String nomeArquivo) throws FileNotFoundException {
-		arquivo = new File(nomeArquivo);
-		if(arquivo.exists())
-			reader = new BufferedReader(new FileReader(arquivo));
-		else
-			throw new FileNotFoundException();
-	}
+    public Leitor(String nomeArquivo) throws FileNotFoundException {
+	arquivo = new File(nomeArquivo);
+	if(arquivo.exists())
+            reader = new BufferedReader(new FileReader(arquivo));
+	else
+            throw new FileNotFoundException();
+    }
 	
-	public String leProximaLinha() throws IOException {
-		return reader.readLine();
-	}
+    public String leProximaLinha() throws IOException {
+        return reader.readLine();
+    }
 	
-	public void fechaArquivo() throws IOException {
-		reader.close();
-	}
+    public void fechaArquivo() throws IOException {
+	reader.close();
+    }
 }
